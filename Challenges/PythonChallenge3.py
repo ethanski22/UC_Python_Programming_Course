fce84634227272a1eb8d6dc259a9b3e07e3543e7
Ethan Skowronski <ethanski22@icloud.com>
2024-03-26 10:07:40 -0400

    Worked a bit on the challenge

--- Challenges/PythonChallenge3.py
+++ Challenges/PythonChallenge3.py
@@ -1,70 +1,46 @@
-# allsteps function
 def allsteps(word):
-    # Create a dictionary to store words indexed by their sorted form
-    word_dict = {}
-    for w in words:
-        sorted_w = ''.join(sorted(w))
-        if sorted_w not in word_dict:
-            word_dict[sorted_w] = []
-        word_dict[sorted_w].append(w)
+	"""
+	Returns a list of all unique, valid step words appearing in a list of English words.
+	
+	Args:
+	- word (str): The input word for which one-step words are to be found.
+	
+	Returns:
+	- list: A list of all one-step words.
+	
+	Examples:
+	>>> allsteps("APPLE")
+	['ALEPPO', 'APPEAL', 'CAPPLE', 'DAPPLE', 'LAPPED', 'LAPPER', 'LAPPET', 'PALPED', 'PAPULE', 'RAPPEL', 'UPLEAP']
+	
+	>>> allsteps("UC")
+	['CUB', 'CUD', 'CUE', 'CUM', 'CUP', 'CUR', 'CUT', 'LUC', 'UCA']
+	
+	>>> allsteps("BEARCAT")
+	['ACERBATE', 'BACTERIA', 'BRACCATE', 'BRACTEAL', 'CARTABLE', 'SCABRATE']
+	"""
+	
+	# Load the list of English words
+	url = "http://raw.githubusercontent.com/eneko/data-repository/master/data/words.txt"
+	from urllib.request import urlopen
+	wordfile = urlopen(url)
+	words = wordfile.read().decode('utf-8').upper().split()
+	
+	# Convert the input word to uppercase
+	word = word.upper()
+	
+	# Initialize a set to store unique one-step words
+	onestep_words = set()
+	
+	# Generate one-step words
+	for i in range(len(word) + 1):
+			for char in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+					new_word = word[:i] + char + word[i+1:]
+					if new_word != word and new_word in words:
+							onestep_words.add(new_word)
+	
+	return sorted(list(onestep_words))
 
-    # Generate one-step words
-    one_step_words = []
-    sorted_word = ''.join(sorted(word))
-    for char in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
-        new_word = sorted_word + char
-        if new_word in word_dict:
-            one_step_words.extend(word_dict[new_word])
-
-    # Remove duplicates and sort the list
-    return sorted(list(set(one_step_words)))
-
-# longest_ladder function
-def longest_ladder(words):
-    # Create a dictionary to store words indexed by their sorted form
-    word_dict = {}
-    for w in words:
-        sorted_w = ''.join(sorted(w))
-        if sorted_w not in word_dict:
-            word_dict[sorted_w] = []
-        word_dict[sorted_w].append(w)
-
-    def dfs(word, visited):
-        visited.add(word)
-        max_ladder = [word]
-        sorted_word = ''.join(sorted(word))
-        for char in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
-            new_word = sorted_word + char
-            if new_word in word_dict:
-                for next_word in word_dict[new_word]:
-                    if next_word not in visited:
-                        ladder = dfs(next_word, visited)
-                        if len(ladder) > len(max_ladder):
-                            max_ladder = ladder
-        visited.remove(word)
-        return [word] + max_ladder
-
-    # Find the longest k-ladder
-    longest_ladder = []
-    visited = set()
-    for word in words:
-        if word not in visited:
-            ladder = dfs(word, visited)
-            if len(ladder) > len(longest_ladder):
-                longest_ladder = ladder
-
-    return longest_ladder
-
-# Starter code to load the list of English words
-url = "http://raw.githubusercontent.com/eneko/data-repository/master/data/words.txt"
-from urllib.request import urlopen
-wordfile = urlopen(url)
-words = wordfile.read().decode('utf-8').upper().split()
-
-# Test cases for allsteps function
-print(allsteps("APPLE"))    # ['ALEPPO', 'APPEAL', 'CAPPLE', 'DAPPLE', 'LAPPED', 'LAPPER', 'LAPPET', 'PALPED', 'PAPULE', 'RAPPEL', 'UPLEAP']
-print(allsteps("UC"))       # ['CUB', 'CUD', 'CUE', 'CUM', 'CUP', 'CUR', 'CUT', 'LUC', 'UCA']
-print(allsteps("BEARCAT"))  # ['ACERBATE', 'BACTERIA', 'BRACCATE', 'BRACTEAL', 'CARTABLE', 'SCABRATE']
-
-# Test case for longest_ladder function
-print(longest_ladder(words))
+# Run the doctests
+if __name__ == "__main__":
+	import doctest
+	doctest.testmod()
